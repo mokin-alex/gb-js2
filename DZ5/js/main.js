41637bfb86d94705a25092c8257a15c547a47b71
Alex Mokin <mr.aleksay@gmail.com>
2020-02-29 17:36:07 +0500

    Vue.js task1 ver.1

--- DZ5/js/main.js
+++ DZ5/js/main.js
@@ -1,31 +1,47 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 const app = new Vue({
-  el: '#app',
-  data: {
-    catalogUrl: '/catalogData.json',
-    products: [],
-    imgCatalog: 'https://placehold.it/200x150'
-  },
-  methods: {
-    getJson(url){
-      return fetch(url)
-        .then(result => result.json())
-        .catch(error => {
-          console.log(error);
-        })
+    el: '#app',
+    data: {
+        catalogUrl: '/catalogData.json',
+        products: [],
+        imgCatalog: 'https://placehold.it/200x150',
+        searchLine: '', //слово для тестирования связки
+        filtered: [],
+        isInvisible: false,
     },
-    addProduct(product){
-      console.log(product.id_product);
-    }
-  },
-  // хук жизненного цикла
-  mounted(){
-    this.getJson(`${API + this.catalogUrl}`)
-      .then(data => {
-        for(let el of data){
-          this.products.push(el);
+    methods: {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                })
+        },
+        addProduct(product) {
+            console.log(product.id_product);
+        },
+        filterGoods() {
+            const regexp = new RegExp(this.searchLine, 'i');
+            this.filtered = this.products.filter(product => regexp.test(product.product_name));
+            //TODO: не понятно теперь как обратиться к :key где лежит наш id
+            this.products.forEach(el => {
+              const block = document.querySelector(`.product-item[data-id="${el.id_product}"]`);
+              if(!this.filtered.includes(el)){
+                block.classList.add('invisible');
+              } else {
+                block.classList.remove('invisible');
+              }
+            })
         }
-      });
-  }
+    },
+    // хук жизненного цикла
+    mounted() {
+        this.getJson(`${API + this.catalogUrl}`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+    }
 });
